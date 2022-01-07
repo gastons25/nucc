a630afa40ab2df69778243bf4e120f909e8d82b1
Gaston Sanchez <gastons@exatec.tec.mx>
2022-01-07 14:51:43 -0600

    dasad

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gastonstec/nuricc
 
 go 1.17
+
+require github.com/gastonstec/utils v0.0.0-20220106191644-91de70d87ba2
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/gastonstec/utils"
+)
+
 func main () {
+<<<<<<< Updated upstream
 	println("Hello Nuri v1.00.00")
+=======
+
+	js:= "{}"
+
+	if utils.IsJSON(js) {
+		println(js)		
+	}
+>>>>>>> Stashed changes
 }
